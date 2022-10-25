95d022d63415105ad3a34349c01737c2f65a02df
HeeSungLee <a73482006@gmail.com>
2022-10-25 13:31:01 +0900

    ADD::Footer-Struct

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import Header from './components/Header/Header.js'
 import Main from './pages/Main.js'
+import Footer from './components/Footer/Footer.js';
 function App() {
   return (
     <>
       <Header>헤더 바</Header>
       <Main>메인</Main>
+      <Footer></Footer>
     </>
   );
 }
---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,23 @@
+import "./Footer.scss"
+
+function Footer(){
+    return(
+        <>
+        <footer className="footer">
+            <div id='footer-con'>
+                <h1>content1</h1>    
+            </div>
+            
+            <div id='footer-con'>
+                <h1>content2</h1>
+            </div>
+            
+            <div id='footer-con'>
+                <h1>content3</h1>
+            </div>
+        </footer>
+        </>
+    )
+}
+
+export default Footer
